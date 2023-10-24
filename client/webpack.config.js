507afaa1c2df5275f3b4aaa4606168cf00137660
Daniel Bonn <danieltbonn@gmail.com>
2023-10-24 18:45:34 -0500

    fixed errors in webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -51,9 +51,7 @@ module.exports = () => {
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             }
           }
-
         }
-        
       ],
     },
   };
